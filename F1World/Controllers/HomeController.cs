df3e32754c7d3ca51dbf9675b0019e85082c2787
Християн Христов <hh13147454@edu.mon.bg>
2025-02-14 15:24:07 +0200

    3ти

--- F1World/Controllers/HomeController.cs
+++ F1World/Controllers/HomeController.cs
@@ -1,20 +1,61 @@
 using F1World.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace F1World.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _httpClient = new HttpClient();
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            // Дефинираме ViewData
+            ViewData["Title"] = "F1 World";
+
+            // 1?? Взимаме F1 Standings от API
+            var standingsUrl = "https://ergast.com/api/f1/current/driverStandings.json";
+            var drivers = new List<dynamic>(); // Празен лист, ако API не върне резултати
+
+            try
+            {
+                var response = await _httpClient.GetStringAsync(standingsUrl);
+                var json = JObject.Parse(response);
+                drivers = json["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"].ToObject<List<dynamic>>();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Грешка при вземане на Standings: " + ex.Message);
+            }
+
+            ViewData["Drivers"] = drivers;
+
+            // 2?? Взимаме F1 Новини (примерен RSS feed)
+            var newsUrl = "https://www.formula1.com/en/latest/all.xml"; // Замени с валиден RSS JSON API
+            string newsHtml = "<p>Грешка при зареждане на новините.</p>";
+
+            try
+            {
+                var newsResponse = await _httpClient.GetStringAsync(newsUrl);
+                newsHtml = $"<pre>{newsResponse}</pre>"; // Тук трябва да обработим XML към HTML
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Грешка при зареждане на новини: " + ex.Message);
+            }
+
+            ViewData["News"] = newsHtml;
+
             return View();
         }
 
